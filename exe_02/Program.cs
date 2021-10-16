61a6b5d2642a1d04d62e572ccdfea037d737c917
Thiago Santos <thiagohillsong@hotmail.com>
2021-10-16 00:55:28 -0300

    exercicio 02 concluido

---
+++ exe_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exe_02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double salario, resultado;
+			Program program = new Program();
+
+			do
+			{
+				Console.WriteLine("### Seja bem vindo(a) a calculadora de salários mínimos! ###");
+				Console.Write("Informe o seu salário: ");
+				salario = Convert.ToDouble(Console.ReadLine());
+				if (salario <= 0)
+				{
+					Console.WriteLine("### Salário precisa ser maior que zero. Informe o salário novamente. ### \n");
+				}
+			} while (salario <= 0);
+
+
+			resultado = program.calculoSalario(salario);
+
+			Console.WriteLine("R${0} é equivalente a {1} salários mínimos.", salario, Math.Round(resultado, 1));
+		}
+
+		public double calculoSalario(double salario)
+		{
+			return (salario / 1100);
+		}
+	}
+}
